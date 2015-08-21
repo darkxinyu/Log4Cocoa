a572430eaa3ef3f1cd717834f0709c36cb708449
wangxy <wangxy@gxhunter.com.cn>
2015-08-21 16:11:49 +0800

    fix crashing in ios8 arm64

--- Log4Cocoa/L4Logging.m
+++ Log4Cocoa/L4Logging.m
@@ -4,6 +4,7 @@
 
 #import <objc/message.h>
 #import "L4Logging.h"
+#import "L4Logger.h"
 #import "NSObject+Log4Cocoa.h"
 
 void log4Log(id object, int line, const char *file, const char *method, SEL sel, L4Level *level, BOOL isAssertion, BOOL assertion,  id exception, id message, ...)
@@ -18,10 +19,17 @@ void log4Log(id object, int line, const char *file, const char *method, SEL sel,
 		combinedMessage = [message retain];
 	}
 	
+	L4Logger *logger = [object l4Logger];
 	if ( isAssertion ) {
-		objc_msgSend([object l4Logger], sel, line, file, method, assertion, combinedMessage);
+		//objc_msgSend([object l4Logger], sel, line, file, method, assertion, combinedMessage);
+		
+		// workaround for crashing in iOS8 64bit
+		[logger lineNumber: line fileName: (char *)file methodName: (char *)method assert: assertion log: combinedMessage];
 	} else {
-		objc_msgSend([object l4Logger], sel, line, file, method, combinedMessage, level, exception);
+		//objc_msgSend([object l4Logger], sel, line, file, method, combinedMessage, level, exception);
+		
+		// workaround for crashing in iOS8 64bit
+		[logger lineNumber:line fileName: (char *)file methodName: (char *)method message: combinedMessage level: level exception: exception];
 	}
 	
 	[combinedMessage release];
